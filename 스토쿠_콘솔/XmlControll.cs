d346926e294ebdbcd477e303301416f35f86dd31
0young1999 <chanqwak@naver.com>
2022-11-09 00:26:09 +0900

    xml 사용 준비

---
+++ 스토쿠_콘솔/XmlControll.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace 스토쿠_콘솔
+{
+	class XmlControll
+	{
+		private static string GetSet(String name)
+		{
+			try
+			{
+				XmlDocument document = new XmlDocument();
+
+				document.Load("Setting.xml");
+
+				XmlNode node = document.SelectSingleNode("Setting");
+
+				return node.Attributes[name].Value;
+			}
+			catch (Exception e)
+			{
+				throw e;
+			}
+		}
+
+		public static string getLastSetFileName()
+		{
+			try
+			{
+				return GetSet("LastSetFileName");
+			}
+			catch (Exception e)
+			{
+				throw e;
+			}
+		}
+
+		public static bool getPrintState()
+		{
+			try
+			{
+				bool printState = false;
+				string printStateString = GetSet("PrintState");
+
+				if (printStateString.Equals("true")) printState = true;
+				else printState = false;
+
+				return printState;
+			}
+			catch (Exception e)
+			{
+				throw e;
+			}
+}
+	}
+}
